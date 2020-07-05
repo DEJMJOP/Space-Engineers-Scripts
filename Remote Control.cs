adb2cbdf18eb7522bb1fcef356a2e82dc78bf7e2
DEJMJOP <dejmjop@hotmail.co.uk>
2020-07-05 09:21:39 +0100

    Added additional function for just returning Vector3D

--- Remote Control.cs
+++ Remote Control.cs
@@ -3,6 +3,7 @@ Takes an input argument of a gps coordinate
 GPS:Ice:1048917.7584688:79752.6323696933:1600020.79087306:
 */
 
+
 public void Main(string args){
 	Echo("Run");
 	//Create a list to store coords
@@ -20,10 +21,9 @@ public void Main(string args){
 	
 	//Create an empty object to store a match
 	System.Text.RegularExpressions.Match match = null;
-	/*
-		Ensures there are actually matches for the expression.
-		Places coordinates into the coords list by parsing strings
-	*/
+	
+	//	Ensures there are actually matches for the expression.
+	//	Places coordinates into the coords list by parsing strings
 	if(matches.Count > 0){
 		match = matches[0];
 		for(int i = 0; i < match.Groups.Count; i++){
@@ -37,9 +37,8 @@ public void Main(string args){
 		Echo("No Match");
 	}
 	
-	/*
-		Adds the waypoint to the remote control block
-	*/
+	
+	//Adds the waypoint to the remote control block
 	if(coords.Count >= 3){
 		remote.ClearWaypoints();
 		Vector3D Pos = new Vector3D();
@@ -51,6 +50,50 @@ public void Main(string args){
 		
 		remote.AddWaypoint(Pos, "Target");
 	}
-	
-	
 }
+
+
+//===========================================================================
+
+/*
+A function that takes a string as an input, and returns a Vector3D gps coordinate 
+as an alternative (for ease of use), if you just want a function that gives you a
+Vector3D output
+*/
+
+Vector3D StringToGPS(string gps){
+	List<float> coords = new List<float>();
+	string pattern = @"GPS:[a-zA-Z0-9_.-]*:(-*\d*.\d*):(-*\d*.\d*):(-*\d*.\d*):";
+	System.Text.RegularExpressions.MatchCollection matches;
+	matches = System.Text.RegularExpressions.Regex.Matches(gps, pattern);
+	System.Text.RegularExpressions.Match match = null;
+	if(matches.Count > 0){
+		match = matches[0];
+		for(int i = 0; i < match.Groups.Count; i++){
+			float coord;
+			if(float.TryParse(match.Groups[i].ToString(), out coord)){
+				coords.Add(coord);
+			}
+		}
+	}
+	if(coords.Count >= 3){
+		Vector3D Pos = new Vector3D();
+		Pos.X = coords[0];
+		Pos.Y = coords[1];
+		Pos.Z = coords[2];
+		return Pos;
+	}
+	Echo("Error! Could Not Find Coordinates!");
+	return new Vector3D(0,0,0);
+}
+
+/*
+Example Main Method to show usage of StringToGPS function
+
+public void Main(){
+	Vector3D test;
+	test = StringToGPS("GPS:Ice:1048917.7584688:79752.6323696933:1600020.79087306:");
+	Echo("X: " + test.X + " Y: " + test.Y + " Z: " + test.Z);
+}
+*/
+
