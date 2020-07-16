479455d60486e4222acc569abf3a8f353f42bdfe
DEJMJOP <dejmjop@hotmail.co.uk>
2020-07-16 20:24:16 +0100

    Created wireless door scripts

---
+++ Open Door - Recieve.cs
@@ -0,0 +1,58 @@
+const string BroadcastTag = "[DRONE BROADCAST]";
+IMyBroadcastListener _bListener;
+IMyTextSurface pbSurface;
+
+public Program()
+{
+	Runtime.UpdateFrequency = UpdateFrequency.Update10;
+	_bListener = IGC.RegisterBroadcastListener(BroadcastTag);
+	_bListener.SetMessageCallback(BroadcastTag);
+	
+	pbSurface = Me.GetSurface(0);
+	pbSurface.ContentType = ContentType.TEXT_AND_IMAGE;
+	pbSurface.FontSize = 1f;
+	pbSurface.Font = "Monospace";
+	pbSurface.Alignment = VRage.Game.GUI.TextPanel.TextAlignment.CENTER;
+	pbSurface.TextPadding = 30f;
+}
+
+public void Main()
+{
+	if(_bListener.HasPendingMessage){
+		HandleMessages();
+	}
+}
+
+
+bool HandleMessages()
+{
+	do
+	{
+		var msg = _bListener.AcceptMessage();
+		string s = ""+msg.Data;		
+		string[] coords = s.Split(':');
+		
+		string ss = "X: " + coords[0] + "\nY: " + coords[1] + "\nZ: " + coords[2];
+		pbSurface.WriteText(ss);
+		
+		Vector3 pos = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
+		
+		FindClosestDoor(pos).OpenDoor();
+	} while(_bListener.HasPendingMessage);
+	return true;
+}
+
+IMyDoor FindClosestDoor(Vector3 pos)
+{
+	List<IMyDoor> doors = new List<IMyDoor>();
+	GridTerminalSystem.GetBlocksOfType<IMyDoor>(doors);
+	IMyDoor closest = doors.ElementAt(0);
+	foreach(IMyDoor d in doors)
+	{
+		if(Vector3.Distance(pos, d.GetPosition()) < Vector3.Distance(pos, closest.GetPosition()))
+			closest = d;
+	}
+	return closest;
+}
+
+
---
+++ Open Door - Send.cs
@@ -0,0 +1,20 @@
+IMyRadioAntenna antenna;
+
+
+const string BroadcastTag = "[DRONE BROADCAST]";
+public Program()
+{
+	antenna = GridTerminalSystem.GetBlockWithName("Antenna") as IMyRadioAntenna;
+}
+
+public void Main(string args)
+{
+	
+	Vector3 pos = antenna.GetPosition();
+	string s = pos.X + ":" + pos.Y + ":" + pos.Z;
+	
+	
+	IGC.SendBroadcastMessage<string>(BroadcastTag,s, TransmissionDistance.AntennaRelay);
+}
+
+
